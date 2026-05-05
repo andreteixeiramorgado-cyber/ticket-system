8fe2ca07da84564cdf91fa5ca01c0ac8cf80610f
andreteixeiramorgado-cyber <andre.teixeira.morgado@gmail.com>
2026-05-05 01:04:27 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,13 +1,20 @@
 const admin = require("firebase-admin");
 
-admin.initializeApp({
-  credential: admin.credential.cert({
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
-  })
-});
+let db;
 
-const db = admin.firestore();
+if (process.env.FIREBASE_PRIVATE_KEY) {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+    })
+  });
+
+  db = admin.firestore();
+  console.log("🔥 Firebase ligado");
+} else {
+  console.log("⚠️ Firebase NÃO configurado (modo teste)");
+}
 
 module.exports = db;
