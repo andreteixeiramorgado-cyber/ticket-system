71644aa625cc83589759653e22e93013961c93a5
andreteixeiramorgado-cyber <andre.teixeira.morgado@gmail.com>
2026-05-05 18:48:05 +0100

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,11 @@ const PORT = process.env.PORT || 3000;
 app.use(session({
   secret: "segredo_super_forte_123",
   resave: false,
-  saveUninitialized: true
+  saveUninitialized: false,
+  cookie: {
+    secure: true,       // 🔥 obrigatório no Render (https)
+    sameSite: "none"
+  }
 }));
 
 app.use(express.json());
